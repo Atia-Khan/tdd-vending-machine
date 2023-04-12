a90b58576fc0476c80124a4872496bfdaedc8c7e
Hunaid baig <hunaid.baig@xloopdigital.com>
2023-04-13 01:18:11 +0500

    My 5th test case failed

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -18,87 +18,104 @@ describe("the vending machine", () => {
     // assert
     expect(expected).toEqual(actual);
   });
-});
 
-//AC 2: As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
-// describe("the vending machine", () => {
-//     const acceptedAmounts = [10, 20, 50, 100, 500];
-//     const depositedMoney = 500;
-// if(acceptedAmounts.includes(depositedMoney)){
-//     it("should tell me how much money I have deposited", () => {
-//         // setup
-//         const machine = new Machine();
-    
-        
-//         const expectedBill = `You have deposited RS: ${depositedMoney}`;
-    
-//         // exercise
-//         const deposited = machine.deposit(depositedMoney);
-    
-//         // assert
-        
-//         expect(deposited).toEqual(expectedBill);
-//       })
-// } else {
-//     it('does not accept invalid bill amount', ()=> {
 
-//         const machine = new Machine();
-    
-//         // const depositedMoney = 200;
-//         const expectedBill = "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
-    
-//             // exercise
-//             const deposited = machine.deposit(depositedMoney);
-    
-//              // assert
-        
-//         expect(deposited).toEqual(expectedBill);
-    
-//       })
-//     }
-    
+  //AC 2: As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
+  // describe("the vending machine", () => {
+  //     const acceptedAmounts = [10, 20, 50, 100, 500];
+  //     const depositedMoney = 500;
+  // if(acceptedAmounts.includes(depositedMoney)){
+  //     it("should tell me how much money I have deposited", () => {
+  //         // setup
+  //         const machine = new Machine();
+      
+          
+  //         const expectedBill = `You have deposited RS: ${depositedMoney}`;
+      
+  //         // exercise
+  //         const deposited = machine.deposit(depositedMoney);
+      
+  //         // assert
+          
+  //         expect(deposited).toEqual(expectedBill);
+  //       })
+  // } else {
+  //     it('does not accept invalid bill amount', ()=> {
 
-// });
+  //         const machine = new Machine();
+      
+  //         // const depositedMoney = 200;
+  //         const expectedBill = "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
+      
+  //             // exercise
+  //             const deposited = machine.deposit(depositedMoney);
+      
+  //              // assert
+          
+  //         expect(deposited).toEqual(expectedBill);
+      
+  //       })
+  //     }
+      
 
-// 2nd test case
-it('show me, how much money I have deposited', () => {
-  // setup
-  const machine = new Machine();
-  const myDeposit = 100;
-  const notAccepted = 1000;
-  // exercise
-  const actualAmount = machine.deposit(myDeposit);
-  const notAcceptedAmount = machine.deposit(notAccepted);
+  // });
 
-  // assert
-  expect(`You have deposited Rs ${myDeposit}`).toEqual(actualAmount);
-  expect(`We accept bills in these amounts: 10, 20, 50, 100, 500`).toEqual(notAcceptedAmount);
-});
+  // 2nd test case
+  it('show me, how much money I have deposited', () => {
+    // setup
+    const machine = new Machine();
+    const myDeposit = 100;
+    const notAccepted = 1000;
+    // exercise
+    const actualAmount = machine.deposit(myDeposit);
+    const notAcceptedAmount = machine.deposit(notAccepted);
 
-// 3rd test case
-it('I want to add additional money', () => {
-  // setup
-  const machine = new Machine();
-  const previousDeposit = 100;
-  const curentDeposit = 50;
+    // assert
+    expect(`You have deposited Rs ${myDeposit}`).toEqual(actualAmount);
+    expect(`We accept bills in these amounts: 10, 20, 50, 100, 500`).toEqual(notAcceptedAmount);
+  });
 
-  machine.deposit(previousDeposit);
-  // exercise
-  const actualAmount = machine.deposit(curentDeposit);
+  // 3rd test case
+  it('I want to add additional money', () => {
+    // setup
+    const machine = new Machine();
+    const previousDeposit = 100;
+    const curentDeposit = 50;
 
-  // assert
-  expect(`You have deposited Rs ${previousDeposit + curentDeposit}`).toEqual(actualAmount);
-});
+    machine.deposit(previousDeposit);
+    // exercise
+    const actualAmount = machine.deposit(curentDeposit);
 
-// 4th test case
-it('I want to see a message if my item is unavailable', () => {
-  // setup
-  const machine = new Machine();
-  const itemCode = 'b4';
+    // assert
+    expect(`You have deposited Rs ${previousDeposit + curentDeposit}`).toEqual(actualAmount);
+  });
 
-  // exercise
-  const receivedItem = machine.selectItem(itemCode);
+  // 4th test case
+  it('I want to see a message if my item is unavailable', () => {
+    // setup
+    const machine = new Machine();
+    const itemCode = 'b4';
 
-  // assert
-  expect(`The item you selected is unavailable`).toEqual(receivedItem);
+    // exercise
+    const receivedItem = machine.selectItem(itemCode);
+
+    // assert
+    expect(`The item you selected is unavailable`).toEqual(receivedItem);
+  });
+
+  // 5th test case
+  it('if my deposit is insufficient', () => {
+    // setup
+    const machine = new Machine();
+    const amountDeposit = 50;
+    const itemCode = 'b3';
+
+    // exercise
+    machine.deposit(amountDeposit);
+    const receivedItem = machine.selectItem(itemCode);
+
+    // assert
+    expect(`Your deposit is insufficient.  Please add Rs ${70-amountDeposit} for this item`).toEqual(receivedItem);
+  });
+
 });
