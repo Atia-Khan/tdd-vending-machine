e0f1d42f111f3687de3b70dc816b8ab8e081a130
Hunaid baig <hunaid.baig@xloopdigital.com>
2023-04-13 00:10:18 +0500

    I have passed my 3rd test case

--- src/Machine.js
+++ src/Machine.js
@@ -1,19 +1,32 @@
 module.exports = class Machine {
   constructor() {
     this.items = [{ crisps: 100 }, { chocolate: 350 }, { mints: 70 }];
+    this.amount = 0;
   }
 
   seeSelections() {
     return this.items;
   }
 
-  deposit(amount) {
 
-    const acceptedAmounts = [10, 20, 50, 100, 500];
-    if (acceptedAmounts.includes(amount)) {
-      return `You have deposited RS: ${amount}`;
-    } else {
-      return "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
+  deposit(myDeposit) {
+    const acceptedBill = [10, 20, 50, 100, 500];
+    if(acceptedBill.includes(myDeposit)){
+        this.amount += myDeposit;
+        return `You have deposited Rs ${this.amount}`;
     }
-  }
+    else{
+        return 'We accept bills in these amounts: 10, 20, 50, 100, 500'
+    }
+}
+
+  // deposit(amount) {
+
+  //   const acceptedAmounts = [10, 20, 50, 100, 500];
+  //   if (acceptedAmounts.includes(amount)) {
+  //     return `You have deposited RS: ${amount}`;
+  //   } else {
+  //     return "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
+  //   }
+  // }
 };
