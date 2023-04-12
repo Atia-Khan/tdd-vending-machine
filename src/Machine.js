1f616235b1abf40f2f9d885304ede6e306a878b4
Atia-Khan <atia.khan@xloopdigital.com>
2023-04-12 23:46:49 +0500

    AC 2 test

--- src/Machine.js
+++ src/Machine.js
@@ -1,13 +1,19 @@
 module.exports = class Machine {
   constructor() {
-    this.items = [
-    {'crisps': 100}, 
-    {'chocolate': 350}, 
-    {'mints': 70}
-    ];
+    this.items = [{ crisps: 100 }, { chocolate: 350 }, { mints: 70 }];
   }
 
   seeSelections() {
     return this.items;
+  }
+
+  deposit(amount) {
+
+    const acceptedAmounts = [10, 20, 50, 100, 500];
+    if (acceptedAmounts.includes(amount)) {
+      return `You have deposited RS: ${amount}`;
+    } else {
+      return "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
+    }
   }
 };
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,15 +1,13 @@
 const Machine = require("../src/Machine");
 // const Items = require("../src/Items");
 
-
-
 // AC 1: As a customer, I want to see that the vending machine has items, so that I can purchase them.
 describe("the vending machine", () => {
   it("should have items to purchase", () => {
     // setup
     const machine = new Machine();
 
-    const expected = [{ "crisps": 100 }, { "chocolate": 350 }, { "mints": 70 }];
+    const expected = [{ crisps: 100 }, { chocolate: 350 }, { mints: 70 }];
 
     // exercise
     const actual = machine.seeSelections();
@@ -22,21 +20,41 @@ describe("the vending machine", () => {
 
 //AC 2: As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
 
-
 describe("the vending machine", () => {
+    const acceptedAmounts = [10, 20, 50, 100, 500];
+    const depositedMoney = 500;
+if(acceptedAmounts.includes(depositedMoney)){
     it("should tell me how much money I have deposited", () => {
-      // setup
-      const machine = new Machine();
-  
-      const depositedMoney = 150;
-      const expectedBill = `You have deposited RS: ${depositedMoney}`;
-  
-      // exercise
-      const deposit = deposit(depositedMoney);
-  
-      // assert
-      expect(typeof deposit).toEqual('string');
-      expect(deposit).toEqual(expectedBill);
-    });
-  });
+        // setup
+        const machine = new Machine();
+    
+        
+        const expectedBill = `You have deposited RS: ${depositedMoney}`;
+    
+        // exercise
+        const deposited = machine.deposit(depositedMoney);
+    
+        // assert
+        
+        expect(deposited).toEqual(expectedBill);
+      })
+} else {
+    it('does not accept invalid bill amount', ()=> {
 
+        const machine = new Machine();
+    
+        // const depositedMoney = 200;
+        const expectedBill = "You have deposited the wrong amount, Machine accept bills only in 10,20,50,100,500 Rupees";
+    
+            // exercise
+            const deposited = machine.deposit(depositedMoney);
+    
+             // assert
+        
+        expect(deposited).toEqual(expectedBill);
+    
+      })
+    }
+    
+
+});
